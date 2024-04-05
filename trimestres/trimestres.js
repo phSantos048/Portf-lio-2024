b75cf85631da62ee49e9123c6cad34e8278785ba
phSantos048 <128161966+phSantos048@users.noreply.github.com>
2024-04-05 09:53:53 -0300

    Add files via upload

---
+++ trimestres/trimestres.js
@@ -0,0 +1,62 @@
+//----1°trimestre----
+
+function voltarParaInicio1() {
+    location.href = '../index.html';
+}
+function irParaHumanas1() {
+    location.href = '../matérias trimestre 1/Ciências Humanas.html';
+}
+function irParaLinguagens1() {
+    location.href = '../matérias trimestre 1/linguagens.html';
+}
+function irParaMatematica1() {
+    location.href = '../matérias trimestre 1/Matemática.html';
+}
+function irParaNatureza1() {
+    location.href = '../matérias trimestre 1/Ciências da Natureza.html';
+}
+function irParaTecnico1() {
+    location.href = '../matérias trimestre 1/Técnico.html';
+}
+
+//----2°trimestre----
+
+function voltarParaInicio2() {
+    location.href = '../index.html';
+}
+function irParaHumanas2() {
+    location.href = '../matérias trimestre 2/Ciências Humanas.html';
+}
+function irParaLinguagens2() {
+    location.href = '../matérias trimestre 2/linguagens.html';
+}
+function irParaMatematica2() {
+    location.href = '../matérias trimestre 2/Matemática.html';
+}
+function irParaNatureza2() {
+    location.href = '../matérias trimestre 2/Ciências da Natureza.html';
+}
+function irParaTecnico2() {
+    location.href = '../matérias trimestre 2/Técnico.html';
+}
+
+//----3°trimestre----
+
+function voltarParaInicio3() {
+    location.href = '../index.html';
+}
+function irParaHumanas3() {
+    location.href = '../matérias trimestre 3/Ciências Humanas.html';
+}
+function irParaLinguagens3() {
+    location.href = '../matérias trimestre 3/linguagens.html';
+}
+function irParaMatematica3() {
+    location.href = '../matérias trimestre 3/Matemática.html';
+}
+function irParaNatureza3() {
+    location.href = '../matérias trimestre 3/Ciências da Natureza.html';
+}
+function irParaTecnico3() {
+    location.href = '../matérias trimestre 3/Técnico.html';
+}
